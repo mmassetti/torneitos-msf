97c4b640bb57faf0655a5d8d6f7a6fffc1409342
Matias Massetti <matiasmassetti@gmail.com>
2021-02-21 17:30:12 -0300

    update queries

---
+++ graphql/queries.js
@@ -0,0 +1,51 @@
+import gql from "graphql-tag";
+
+export const GET_TEMPORADAS = gql`
+  {
+    allTemporadas {
+      data {
+        _id
+        nombre
+      }
+    }
+  }
+`;
+
+export const GET_TORNEOS_PARA_TEMPORADA = gql`
+  query GetTorneosParaTemporada($nombre: String!) {
+    temporadaByName(nombre: $nombre) {
+      nombre
+      torneos {
+        data {
+          _id
+          numeroTorneo
+          equipoMasa
+          equipoChaca
+          equipoSeba
+          ganador
+          resultados {
+            data {
+              jugador1
+              jugador2
+              golesJugador1
+              golesJugador2
+              numeroEnfrentamiento
+            }
+          }
+          tablas {
+            data {
+              jugador
+              puntos
+              pj
+              pg
+              pe
+              pp
+              gf
+              gc
+            }
+          }
+        }
+      }
+    }
+  }
+`;
